423956d3ebe9d3914dbd56f5f6bb27a4ef7ef36f
rafael-tumasov <gold005dev@gmail.com>
2024-01-25 16:49:11 -0500

    #8

---
+++ routes/product.js
@@ -0,0 +1,26 @@
+const router = rootRequire('helpers/router.js');
+const { renderTemplate } = rootRequire('helpers/utils.js');
+
+router.post('/product',
+  (ctx, next) => {
+    const { CrawlingAPI } = require('proxycrawl');
+    const data = ctx.request.body;
+    const api = new CrawlingAPI({ token: data.token });
+
+    return api
+      .get(data.url, { scraper: 'amazon-product-details' })
+      .then(response => {
+        if (response.statusCode !== 200) {
+          throw new Error('Invalid ProxyCrawl status code: ' + response.statusCode + '\n' + response.body);
+        } else {
+          ctx.product = JSON.parse(response.body).body;
+        }
+        next();
+      })
+      .catch(err => {
+        console.error(err);
+        ctx.error = err;
+        next();
+      });
+  },
+  (ctx) => ctx.body = renderTemplate('product', { error: ctx.error, product: ctx.product }));
